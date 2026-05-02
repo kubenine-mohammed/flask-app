46f4eef13ba43447d0e44791b226e0ee199f41cd
Mohammed Shafi A <m.shafi@kubenine.com>
2026-05-02 11:02:36 +0530

    updated v1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return {"status": "ok"}
+    return {"status": "updated v1"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
