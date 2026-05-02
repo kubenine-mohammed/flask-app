ff3f1bd38d7b6922fc855f9409db0dd152dd5a82
kubenine-mohammed <m.shafi@kubenine.com>
2026-05-02 05:31:36 +0000

    simple flask app

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"status": "ok", "message": "Hello from EKS!"})
-
-@app.route("/hello")
-def hello():
-    return jsonify({
-        "status": "ok",
-        "message": "Hello endpoint working!"
-    })
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
